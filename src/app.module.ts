8e77a1130940c14ca293087304677c9d3ac86900
Utku Demir <utkdmr1@gmail.com>
2023-06-18 01:55:30 +0300

    feat(place-module): create place module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { TypeOrmModule } from '@nestjs/typeorm'; 
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmAsyncConfig } from './lib/config';
 import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
 import { MenuModule } from './menu/menu.module';
+import { PlaceModule } from './place/place.module';
 
 @Module({
-  imports: [UserModule,
+  imports: [
+    UserModule,
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
     ProductModule,
     OrderModule,
-    MenuModule,],
+    MenuModule,
+    PlaceModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
